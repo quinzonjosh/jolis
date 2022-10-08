1bfa692c06fcd6816e7727e59f189851d7632591
Josh Quinzon <quinzonjosh261@gmail.com>
2022-10-08 22:51:11 +0800

    updated navbar.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,23 +1,22 @@
 const Footer = () => {
   return (
     <div>
-      <h1>JOLI'S</h1>
-        <div className="overview_container">
-          Overview
-          <a href="">Home</a>
-          <a href="">Products</a>
-          <a href="">Our branches</a>
-          <a href="">Contact us</a>
-        </div>
-        <div className="our_products_container">
-          <a href="">Architecture</a>
-          <a href="">School Supplies</a>
-          <a href="">Drawing Supplies</a>
-          <a href="">Tables</a>
-        </div>
-        <div className="footer_contact_us">
-          <img src="" alt="facebook logo"/>
-        </div>
+      <div className="overview_container">
+        Overview
+        <a href="">Home</a>
+        <a href="">Products</a>
+        <a href="">Our branches</a>
+        <a href="">Contact us</a>
+      </div>
+      <div className="our_products_container">
+        <a href="">Architecture</a>
+        <a href="">School Supplies</a>
+        <a href="">Drawing Supplies</a>
+        <a href="">Tables</a>
+      </div>
+      <div className="footer_contact_us">
+        <img src="" alt="facebook logo" />
+      </div>
     </div>
   );
 };
