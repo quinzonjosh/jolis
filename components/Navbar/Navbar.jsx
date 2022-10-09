8c9f29ae09eb9af5084fe1daf73ebe857d344f48
Josh Quinzon <quinzonjosh261@gmail.com>
2022-10-10 00:42:52 +0800

    added mobile navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import { GrSearch } from "react-icons/gr";
-
+import { useState } from "react";
+import styles from "./Navbar.module.css";
 const Navbar = () => {
+  const [hideNav, setHideNav] = useState(true);
+
   return (
     <section className="flex justify-center w-full gap-2 py-5 items-center border-b-2 border-black">
-      <span className="text-3xl font-bold">JOLI'S</span>
+      <span className="text-3xl font-bold ">JOLI'S</span>
 
       <div className="flex w-[60%]">
         <input
@@ -17,24 +20,46 @@ const Navbar = () => {
           <GrSearch />
         </button>
       </div>
+      <button
+        onClick={() => {
+          setHideNav((hideNav) => {
+            return !hideNav;
+          });
+        }}
+      >
+        Open
+      </button>
 
-      <ul className="flex flex-row gap-2">
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Products</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Get in touch</a>
-          </Link>
-        </li>
-      </ul>
+      <div
+        className={`${hideNav ? "-left-full" : "left-0"} ${styles.nav_links}`}
+      >
+        <ul className="flex flex-col md:flex-row gap-2">
+          <button
+            onClick={() => {
+              setHideNav((hideNav) => {
+                return !hideNav;
+              });
+            }}
+          >
+            Close
+          </button>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Products</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Get in touch</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
