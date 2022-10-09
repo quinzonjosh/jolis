b64e43bc1de2419045865997b7979f2ad405a326
Josh Quinzon <quinzonjosh261@gmail.com>
2022-10-09 23:57:28 +0800

    added products w/ sample image

--- components/Product/Product.jsx
+++ components/Product/Product.jsx
@@ -1,15 +1,30 @@
-const Product = () => {
+import Image from "next/image";
+
+function Product({ prop }) {
   return (
-    <div class="product_container">
-      <img src="" alt="" />
-      <span id="product_name"></span>
-      <span id="product_category"></span>
-      Stock status Espana Branch
-      <span id="product_stock_status_espana"></span>
-      <span id="product_stock_status_pnoval"></span>
-      P. Noval Branch
+    <div className="block max-w-[310px] p-6 border border-black">
+      <div className="relative w-64 h-64">
+        <Image src="/images/sample.jpg" alt="sample" layout="fill" />
+      </div>
+
+      <h3 className="font-bold break-words">Product name</h3>
+      <p className="mb-3">Product Category</p>
+      <div className="flex flex-row gap-5">
+        <div className="flex flex-col">
+          <div className="font-bold">Espana Branch</div>
+          <div className="bg-zinc-200 rounded-xl px-1 text-center">
+            In stock
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="font-bold">P. Noval Branch</div>
+          <div className="bg-zinc-200 rounded-xl px-3 text-center">
+            Out of Stock
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Product;
