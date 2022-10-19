d5680ef0efde283e900ef482c34abf731cda5272
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2022-10-19 16:49:07 +0800

    Added slugs and pics to categories

--- components/Category/Category.jsx
+++ components/Category/Category.jsx
@@ -1,12 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link';
 
-const Category = ({imageLink, categoryName}) =>{
+const Category = ({imageLink, categoryName, slug}) =>{
     return (
-        <Link href={{
-            pathname: "/productListing",
-            query: {categoryName}
-        }}>
+        <Link href={`/categories/${slug}`}>
             <div className="category_container text-center flex flex-col items-center hover:font-bold hover:text-blue-400">
                 <div className='relative w-64 h-64'>
                     <Image layout='fill' priority={true} src={imageLink} alt="category image" />
