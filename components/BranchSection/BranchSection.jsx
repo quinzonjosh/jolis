3191691c641e81721c60111e650455730af6b93f
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2022-11-08 03:31:24 +0800

    Changed branch info to match new design, maps embedded to branches

--- components/BranchSection/BranchSection.jsx
+++ components/BranchSection/BranchSection.jsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 const BranchInfo = ({
   branchName,
   facebookLink,
-  phoneNumber,
+  mobile,
+  tel,
+  printing,
+  email,
   address,
+  hours,
   mapFirst,
 }) => {
   return (
@@ -13,42 +17,53 @@ const BranchInfo = ({
         mapFirst ? "order-2 md:w-2/5" : "order-1 md:1/2"
       }`}
     >
-      <p className="text-2xl font-bold mb-10 lg:mx-8">{branchName}</p>
-      <p className="text-xl font-bold mb-10 lg:mx-8">Reach Us</p>
-      <span className="mb-10 lg:mx-8">
-        <p className="float-left mr-12">Facebook</p>
+      <p className="text-2xl font-bold mb-5 lg:mx-8">{branchName}</p>
+      <p className="text-xl font-bold mb-5 lg:mx-8">Reach Us</p>
+      <span className="mb-5 lg:mx-8">
+        <p className="float-left mr-12">Facebook:</p>
         <a
           href={facebookLink}
-          className="w-1/2 md:w-2/4 hover:text-blue-400 break-words float-left"
+          className="w-1/2 md:w-2/4 hover:text-blue-400 break-words float-left relative left-12"
         >
           {facebookLink}
         </a>
       </span>
-      <p className="mb-10 lg:mx-8">
-        <a className="float-left mr-20">Phone</a>
-        <a className="float-left relative right-2">{phoneNumber}</a>
+      <p className="mb-5 lg:mx-8">
+        <a className="float-left mr-20">Mobile:</a>
+        <a className="float-left relative left-8">{mobile}</a>
       </p>
-      <span className="mb-10 lg:mx-8">
-        <a className="float-left mr-14">Address</a>
-        <a className="w-2/4 break-words float-left">{address}</a>
+      <p className="mb-5 lg:mx-8">
+        <a className="float-left mr-20">Tel:</a>
+        <a className="float-left relative left-16">{tel}</a>
+      </p>
+      <p className="mb-5 lg:mx-8">
+        <a className="float-left mr-20">Printing:</a>
+        <a className="float-left relative left-7">{printing}</a>
+      </p>
+      <p className="mb-5 lg:mx-8">
+        <a className="float-left mr-20">Email:</a>
+        <a className="float-left relative left-12">{email}</a>
+      </p>
+      <span className="mb-5 lg:mx-8">
+        <a className="float-left mr-14">Address:</a>
+        <a className="w-2/4 break-words float-left relative left-12">{address}</a>
+      </span>
+      <span className="mb-5 lg:mx-8">
+        <a className="float-left mr-14">Opening hours:</a>
+        <a className="w-2/5 break-words float-left">{hours}</a>
       </span>
     </div>
   );
 };
 
-const BranchMap = ({ mapFirst }) => {
+const BranchMap = ({ mapFirst, mapEmbed }) => {
   return (
     <div
-      className={`relative w-full md:w-2/5 h-96 lg:mx-8 ${
+      className={`relative lg:md:w-2/5 md:md:w-2/4 w-full h-96 lg:mx-8 ${
         mapFirst ? "order-2 md:order-1" : "order-2"
       }`}
     >
-      <Image
-        layout="fill"
-        priority="true"
-        src="/images/default.jpg"
-        alt="branch image"
-      />
+      <div dangerouslySetInnerHTML={mapEmbed}/>
     </div>
   );
 };
@@ -56,22 +71,31 @@ const BranchMap = ({ mapFirst }) => {
 const BranchSection = ({
   branchName,
   facebookLink,
-  phoneNumber,
+  mobile,
+  tel,
+  printing,
+  email,
   address,
+  hours,
   mapFirst,
+  mapEmbed
 }) => {
   return (
-    <section className="border-b-2 border-black">
+    <section className={`${mapFirst ? "bg-gray-200" : "border-b-2 border-black"}`}>
       <div className="container px-10 md:px-0 mx-auto py-10 flex flex-col items-center md:flex-row md:items-start justify-between">
         <BranchInfo
           branchName={branchName}
           facebookLink={facebookLink}
-          phoneNumber={phoneNumber}
+          mobile={mobile}
+          tel={tel}
+          printing={printing}
+          email={email}
           address={address}
+          hours={hours}
           mapFirst={mapFirst}
         />
         {/* TODO: add link embedd to google maps here */}
-        <BranchMap mapFirst={mapFirst} />
+        <BranchMap mapFirst={mapFirst} mapEmbed={mapEmbed} />
       </div>
     </section>
   );
@@ -80,9 +104,13 @@ const BranchSection = ({
 BranchSection.defaultProps = {
   branchName: "Location Branch",
   facebookLink: "XXXXXXXXXXXXXX",
-  phoneNumber: "09xx xxxx xxx",
+  mobile: "0917-8039316",
+  tel: "8731-3628 / 7902-7030",
+  printing: "8731-9704",
+  email: "jolisgen2@gmail.com",
   address: "XXXXXXXXXXXXXX",
-  reverse: false,
+  hours: `Mon-Sat: 8:30AM-6:00PM,
+  Sunday: Closed`
 };
 
 export default BranchSection;
--- pages/branches.js
+++ pages/branches.js
@@ -5,22 +5,26 @@ import Link from "next/link";
 import BranchSection from "../components/BranchSection/BranchSection";
 
 const branches = () => {
+  const espanaBranch = {__html: '<div class="mapouter"><div class="gmap_canvas"><iframe width="600px" height="400px" id="gmap_canvas" src="https://maps.google.com/maps?q=1567-69%20Espa%C3%B1a%20Blvd.%20Sampaloc%20Mla.%201000%20Manila,%20Philippines&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:absolute;text-align:right;height:100%;width:100%;top:0;left:0;border:0}</style><a href="https://www.embedgooglemap.net">how to add a google map to wordpress</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style></div></div>'};
+
   return (
     <Layout>
       <Banner title="Get in touch with us" />
 
       <BranchSection
         branchName="Espana Branch"
-        facebookLink="https://www.facebook.com/jolis.espana"
+        facebookLink="/jolis.espana"
         address="1567-69 España Blvd. Sampaloc Mla. 1000 Manila, Philippines"
         mapFirst={false}
+        mapEmbed={espanaBranch}
       />
 
       <BranchSection
         branchName="P.Noval Branch"
-        facebookLink="https://www.facebook.com/jolis.pnoval"
+        facebookLink="/jolis.pnoval"
         address="1567-69 España Blvd. Sampaloc Mla. 1000 Manila, Philippines"
         mapFirst={true}
+        mapEmbed={espanaBranch}
       />
     </Layout>
   );
