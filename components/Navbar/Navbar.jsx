9f85db8ecfacacef0963f0f287287ffd6bf52d0d
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-11-15 14:45:35 +0800

    fix typography and style searchbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
   const [hideNav, setHideNav] = useState(true);
 
   return (
-    <section className="flex justify-center w-full gap-2 py-5 items-center border-b-2 border-black">
+    <header className="flex justify-center w-full gap-4 py-5 items-center border-b-2 border-black">
       <Link href="/">
-        <span className="md:text-4xl text-2xl font-bold cursor-pointer">
-          <a>JOLI&apos;S</a>
-        </span>
+        <a className="md:text-4xl text-2xl font-bold cursor-pointer font-heading text-primary">
+          JOLI&apos;S
+        </a>
       </Link>
       {/* search bar */}
       <div className="flex w-[60%]">
@@ -20,10 +20,10 @@ const Navbar = () => {
           type="search"
           name="Search here"
           placeholder="Search products"
-          className="w-[100%] border-2 border-black border-r-0 p-2 text-xs"
+          className={styles.searchbar}
         />
-        <button className="border-2 border-l-0 border-black p-1">
-          <GrSearch />
+        <button className="border-2 border-l-0 border-white-accent-lavender rounded-r-lg p-1">
+          <GrSearch className={styles.search_icon} />
         </button>
       </div>
       <button
@@ -37,10 +37,10 @@ const Navbar = () => {
         <GiHamburgerMenu />
       </button>
 
-      <div
+      <nav
         className={`${hideNav ? "-left-full" : "left-0"} ${styles.nav_links}`}
       >
-        <ul className="flex flex-col md:flex-row gap-2 p-5 text-xl">
+        <ul className="flex flex-col md:flex-row gap-2 p-5">
           <button
             className="md:hidden block self-end"
             onClick={() => {
@@ -53,28 +53,28 @@ const Navbar = () => {
           </button>
           <li>
             <Link href="/">
-              <a className="lg:text-xl md:text-base text:xl relative lg:top-0 md:top-6 top-0 lg:right-0 md:right-4 right-0">
+              <a className="text-base  relative lg:top-0 md:top-6 top-0 lg:right-0 md:right-4 right-0">
                 Home
               </a>
             </Link>
           </li>
           <li>
             <Link href="/categories">
-              <a className="lg:text-xl md:text-base text:xl relative lg:top-0 md:top-6 top-0 lg:right-0 md:right-4 right-0">
+              <a className="text-base relative lg:top-0 md:top-6 top-0 lg:right-0 md:right-4 right-0">
                 Products
               </a>
             </Link>
           </li>
           <li>
             <Link href="/branches">
-              <a className="lg:text-xl md:text-base text:xl relative lg:right-0 md:right-4 right-0">
+              <a className="text-base relative lg:right-0 md:right-4 right-0">
                 Get in touch
               </a>
             </Link>
           </li>
         </ul>
-      </div>
-    </section>
+      </nav>
+    </header>
   );
 };
 
