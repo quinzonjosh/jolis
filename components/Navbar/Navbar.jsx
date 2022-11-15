c64f97014d313e94819efbe9d093eb93673983bb
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-11-15 18:45:37 +0800

    fix alignment and add scroll transition

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
 import Link from "next/link";
+import { useEffect } from "react";
+
+import styles from "./Navbar.module.css";
+
 import { GrSearch } from "react-icons/gr";
 import { useState } from "react";
-import styles from "./Navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
+
 const Navbar = ({ page }) => {
   const [hideNav, setHideNav] = useState(true);
+  const [navStyle, setNavStyle] = useState("initial");
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    setNavStyle(scrollY > 0 ? "scroll" : "initial");
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const links = [
     {
       name: "Home",
@@ -22,66 +39,68 @@ const Navbar = ({ page }) => {
   ];
 
   return (
-    <header className="flex justify-center w-full gap-4 py-5 items-center shadow-md">
-      <Link href="/">
-        <a className="md:text-4xl text-2xl font-bold cursor-pointer font-heading text-primary">
-          JOLI&apos;S
-        </a>
-      </Link>
-      {/* search bar */}
-      <div className="flex w-[60%]">
-        <input
-          type="search"
-          name="Search here"
-          placeholder="Search products"
-          className={styles.searchbar}
-        />
-        <button className="border-2 border-l-0 border-white-accent-lavender rounded-r-lg p-1">
-          <GrSearch className={styles.search_icon} />
-        </button>
-      </div>
-      <button
-        className="md:hidden block text-primary"
-        onClick={() => {
-          setHideNav((hideNav) => {
-            return !hideNav;
-          });
-        }}
-      >
-        <GiHamburgerMenu />
-      </button>
-
-      <nav
-        className={`${hideNav ? "-left-full" : "left-0"} ${styles.nav_links}`}
-      >
-        <ul className="flex flex-col md:flex-row gap-6 p-5 ">
-          <button
-            className="md:hidden block self-end text-primary"
-            onClick={() => {
-              setHideNav((hideNav) => {
-                return !hideNav;
-              });
-            }}
-          >
-            <AiOutlineClose />
+    <header className={styles.navbar} data-style={navStyle}>
+      <div className="flex justify-center w-full gap-6 md:gap-0 py-5 items-center">
+        <Link href="/">
+          <a className={styles.nav_logo}>JOLI&apos;S</a>
+        </Link>
+        {/* search bar */}
+        <div className="flex w-[50%] lg:w-[55%]">
+          <input
+            type="search"
+            name="Search here"
+            placeholder="Search products"
+            className={styles.searchbar}
+          />
+          <button className="border-2 border-l-0 bg-white border-white-accent-lavender rounded-r-lg p-1">
+            <GrSearch className={styles.search_icon} />
           </button>
-          {links.map(({ name, link }) => {
-            return (
-              <li key={name}>
-                <Link href={link}>
-                  <a
-                    className={`${page === name ? styles.nav_active : ""} ${
-                      styles.nav_link
-                    }`}
-                  >
-                    {name}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+        </div>
+        <button
+          className={styles.hamburger_menu}
+          onClick={() => {
+            document.body.style.overflow = "hidden";
+            setHideNav((hideNav) => {
+              return !hideNav;
+            });
+          }}
+        >
+          <GiHamburgerMenu />
+        </button>
+
+        <nav
+          className={`${hideNav ? "-left-full" : "left-0"} ${styles.nav_links}`}
+        >
+          <ul className="flex flex-col md:flex-row md:justify-center p-5 md:p-0 gap-6">
+            <button
+              className="md:hidden text-2xl block self-end text-primary"
+              onClick={() => {
+                document.body.style.overflow = "scroll";
+                setHideNav((hideNav) => {
+                  return !hideNav;
+                });
+              }}
+            >
+              <AiOutlineClose />
+            </button>
+            {links.map(({ name, link }) => {
+              return (
+                <li key={name}>
+                  <Link href={link}>
+                    <a
+                      className={`${page === name ? styles.nav_active : ""} ${
+                        styles.nav_link
+                      }`}
+                    >
+                      {name}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
