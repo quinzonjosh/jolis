6ceac1e2bfb27b87889557f5c229b5e65db0a76d
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2022-11-22 19:14:35 +0800

    Update cleanData.js

--- utils/cleanData.js
+++ utils/cleanData.js
@@ -13,10 +13,12 @@ export const cleanCategories = (rawData) => {
 
 export const cleanProducts = (rawData) => {
         const cleanProduct = rawData.map((products) => {
+                
                 const {sys, fields} = products
                 const {id} = sys
                 const {name, category: categoryobject, pnoval_stock, espana_stock, featured, image: imageUrl} = fields
-                const {categoryName: category} = categoryobject.fields
+                //const {categoryName: category} = categoryobject.fields
+                const category = categoryobject[0].fields.categoryName
                 const image = "https:" + imageUrl.fields.file.url
                 const updatedProducts = {id, name, category, pnoval_stock, espana_stock, featured, image}
                 return updatedProducts
