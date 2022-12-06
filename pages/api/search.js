31d1cda5ff8574432ad15fb5ed57c631ca130e41
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2022-12-07 02:00:26 +0800

    Imported cleanCategories

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Client } from "../../api/contentful";
-import { cleanProducts } from "../../utils/cleanData";
+import { cleanCategories, cleanProducts } from "../../utils/cleanData";
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
